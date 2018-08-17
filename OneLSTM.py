c068575a018accdb8132e38211e102335f62b2f9
Luca Vasapolli <vasapolliluca@gmail.com>
2018-08-17 15:13:35 +0200

    Correzione classe Cerebro

--- OneLSTM.py
+++ OneLSTM.py
@@ -16,7 +16,6 @@ N_LAYERS = 1                                                     # numero di liv
 N_CATEGORIES = 2                                                 # categorie da classificare
 LEARNING_RATE = 0.01                                             # tasso di apprendimento
 BATCH = 53                                                       # domanda ??????????????????????????????
-N_DIRECTION = 1                                                  # 2 per LSTM bidirectional
 BIDIRECTIONAL = False                                            # True per LSTM bidirectional
 EPOCHS = 100                                                     # cicli per ogni time_series
 DEVICE = torch.device('cpu')                                     # cuda per GPU
@@ -26,19 +25,29 @@ TRAIN_DATA = ''
 
 
 class Cerebro(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, input_size, hidden_state, num_layers, bdirectional, batch, num_categories):
         super(Cerebro, self).__init__()
+        self.input_size = input_size
+        self.hidden_state = hidden_state
+        self.num_layer = num_layers
+        self.bdirectional = bdirectional
+        self.batch = batch
+        self.num_categories = num_categories
         # 1° livello
-        self.lstm = nn.LSTM(INPUT_SIZE, H_STATE, N_LAYERS, bidirectional=BIDIRECTIONAL)
+        self.lstm = nn.LSTM(input_size, hidden_state, num_layers, bidirectional=bdirectional)
         # 2° livello  input = n_channel * hidden state (h_size = TIME, output= n_classi)
-        self.fc = nn.Linear(H_STATE * N_CHANNEL, N_CATEGORIES)
+        self.fc = nn.Linear(hidden_state, num_categories)
         # 3° livello classificatore softmax (logSoftmax per poter usare NLLoss)
         self.softmax = nn.LogSoftmax()
         # self.dropout_layer = nn.Dropout(p=0.2)
 
     def forward(self):
-        h_0 = autograd.Variable(torch.zeros(N_LAYERS * N_DIRECTION, BATCH, H_STATE))
-        c_0 = autograd.Variable(torch.zeros(N_LAYERS * N_DIRECTION, BATCH, H_STATE))
+        if self.bdirectional:
+            direction = 2
+        else:
+            direction = 1
+        h_0 = autograd.Variable(torch.zeros(self.num_layer * direction, self.batch, self.hidden_state))
+        c_0 = autograd.Variable(torch.zeros(self.num_layer * direction, self.batch, self.hidden_state))
         output, (h_n, c_n) = self.lstm(input, (h_0, c_0))
         # output = self.dropout_layer(hn[-1])
         output = self.fc(output)
@@ -48,6 +57,7 @@ class Cerebro(torch.nn.Module):
 # Caricamento File funzionante
 # mat = loadmat(FOLDER_DATASET + '/CenterSpeller_VPiac.mat_1_r_1.mat')
 # mat = torch.from_numpy((mat['val']))
+
 
 class CerebroData(Dataset):
     def __init__(self, folder_dataset, mat_file, size=(N_CHANNEL, INPUT_SIZE)):
@@ -65,8 +75,8 @@ class CerebroData(Dataset):
         return mat, label
 
 
-X = Cerebro()#.cuda             # on GPU
-optimizer = torch.optim.Adam(X.parameters(), lr=LEARNING_RATE, momentum=0.9)
+X = Cerebro(INPUT_SIZE, H_STATE, N_LAYERS, BIDIRECTIONAL, BATCH, N_CATEGORIES)#.cuda             # on GPU
+optimizer = torch.optim.Adam(X.parameters(), lr=LEARNING_RATE)
 criterion = nn.NLLLoss()
 dset_train = CerebroData(FOLDER_DATASET)
 train_loader = DataLoader(dset_train, batch_size=500, shuffle=True, num_workers=1)
