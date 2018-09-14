894ed182d4bdaf26ce9922d1de364e1479bc1f90
Luca Vasapolli <vasapolliluca@gmail.com>
2018-09-14 17:11:44 +0200

    Update OneLSTM.py

--- OneLSTM.py
+++ OneLSTM.py
@@ -113,7 +113,6 @@ class CerebroDataset(Dataset):
 model = Cerebro(N_CHANNEL, H_STATE, N_LAYERS, BIDIRECTIONAL, N_CATEGORIES)
 if torch.cuda.device_count() > 1:
     print("Let's use", torch.cuda.device_count(), "GPUs!")
-    # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
     model = nn.DataParallel(model)
 model.to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
